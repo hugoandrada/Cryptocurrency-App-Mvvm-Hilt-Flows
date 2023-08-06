2b2a61e075f4727195525ab613f4f3892911a882
hugo andrada <hugoandrada77@gmail.com>
2023-08-06 16:03:29 -0300

    update code

--- app/src/main/java/com/hugo/andrada/dev/cryptocurrencyapp/presentation/coin_list/HomeFragment.kt
+++ app/src/main/java/com/hugo/andrada/dev/cryptocurrencyapp/presentation/coin_list/HomeFragment.kt
@@ -1,33 +1,33 @@
 package com.hugo.andrada.dev.cryptocurrencyapp.presentation.coin_list
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.hugo.andrada.dev.cryptocurrencyapp.R
+import com.hugo.andrada.dev.cryptocurrencyapp.common.base.BaseFragment
 import com.hugo.andrada.dev.cryptocurrencyapp.databinding.FragmentHomeBinding
 import com.hugo.andrada.dev.cryptocurrencyapp.presentation.adapter.CoinListAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class HomeFragment : Fragment(R.layout.fragment_home) {
+class HomeFragment : BaseFragment<FragmentHomeBinding>() {
+
+    override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentHomeBinding
+        get() = FragmentHomeBinding::inflate
 
     private val viewModel: CoinListViewModel by viewModels()
     private lateinit var coinAdapter: CoinListAdapter
 
-    private var currentBinging: FragmentHomeBinding? = null
-    private val binding get() = currentBinging!!
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        currentBinging = FragmentHomeBinding.bind(view)
         coinAdapter = CoinListAdapter()
         setupAdapter()
         collectCoins()
@@ -54,10 +54,5 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
                 setHasFixedSize(true)
             }
         }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        currentBinging = null
     }
 }
